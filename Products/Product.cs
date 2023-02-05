709e52955f08d0d96ad6308e60779b515fdab052
Matheus Ávila <matheusjhavila@gmail.com>
2023-02-05 11:38:06 -0300

    POO Training

---
+++ Products/Product.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace Tasks
+{
+    internal class Product
+    {
+        public string Name;
+        public double Price;
+        public int Quantity;
+
+        public double TotalInStock()
+        {
+            return Price * Quantity;
+        }
+
+        public override string ToString()
+        {
+            return Name
+            + ", $ "
+            + Price.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantity.ToString("F2", CultureInfo.InvariantCulture)
+            +" units, total: $"
+            + TotalInStock().ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+    }
+}
---
+++ Products/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+
+            Console.WriteLine("Enter product data:");
+
+            Console.Write("Name:");
+            product1.Name = Console.ReadLine();
+            Console.Write("Price:");
+            product1.Price = double.Parse(Console.ReadLine());
+            Console.Write("Quantity in stock:");
+            product1.Quantity = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Product data:" + product1);
+
+
+        }
+    }
+}

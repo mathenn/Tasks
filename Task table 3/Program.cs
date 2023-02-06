c1753dd59e303c5d51397b07a2b32a55be387f43
Matheus Ávila <matheusjhavila@gmail.com>
2023-02-06 10:03:03 -0300

    Task table 3 (POO Training)

---
+++ Task table 3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Task_table_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+
+            Console.WriteLine("Enter the width value:");
+            rectangle.Width = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Enter the height value:");
+            rectangle.Height = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Area = " + rectangle.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Perimeter = " + rectangle.Perimeter().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Diagonal = " + rectangle.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
---
+++ Task table 3/Rectangle.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace Task_table_3
+{
+    internal class Rectangle
+    {
+        public double Width;
+        public double Height;
+
+        public double Area()
+        {
+            return Width * Height;
+        }
+
+        public double Perimeter()
+        {
+            return 2 * (Width + Height);
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Width * Width + Height * Height);
+        }
+        
+    }
+}

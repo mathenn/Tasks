827858fc56d24990345771d43f35eba877aa5f7c
Matheus Ávila <matheusjhavila@gmail.com>
2023-02-14 13:04:22 -0300

    More one task table (Done)

---
+++ Task table 4/BankAccount.cs
@@ -0,0 +1,55 @@
+﻿using System.Globalization;
+
+namespace Task_table_4
+{
+    internal class BankAccount
+    {
+
+        public int AccNumber { get; private set; }
+
+        public string Name { get; set; }
+
+        public double Balance { get; private set; }
+
+        public BankAccount(int accNumber, string name)        /* Constructor */
+        {
+            AccNumber = accNumber;
+            Name = name;
+        }
+
+        public BankAccount(int accNumber, string name, double balance): this(accNumber, name)
+        {
+            Balance = balance;
+        }
+            
+    public void Deposit(double deposit)
+        {
+            if (deposit <= 0)
+            {
+                throw new ArgumentException("Deposit amount can't be 0.");
+            }
+            else
+            {
+                Balance += deposit;
+                Console.WriteLine("Deposit succeed!");
+            }
+        }
+
+        public void Withdraw(double balance)
+        {
+            Balance -= balance + 5.00;
+        }
+
+        public override string ToString()
+        {
+            return "Conta: "
+            + AccNumber
+            + ", Owner: "
+            + Name
+            + ", Balance: $"
+            + Balance.ToString("F2", CultureInfo.InvariantCulture);
+        }
+   
+    }
+}
+
---
+++ Task table 4/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+using Task_table_4;
+
+namespace MyTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount acc1;
+
+            Console.Write("Enter the account number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            Console.Write("Enter the name of the account owner: ");
+            Console.Write("");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("");
+            Console.Write("There will be initial deposit? Type 'Y' for yes or 'N' for no. ");
+            Console.WriteLine("");
+
+            char res = char.Parse(Console.ReadLine().ToUpper());
+            if (res == 'Y')
+            {
+                Console.Write("What is the amount of the initial deposit? ");
+                double balance = double.Parse(Console.ReadLine());
+                acc1 = new BankAccount(number, name, balance);
+                Console.WriteLine("");
+            } else
+            {
+                acc1 = new BankAccount(number, name);
+            }
+
+            Console.WriteLine("Updating your data...");
+            Console.WriteLine("");
+            Console.WriteLine("Account info:");
+            Console.WriteLine(acc1);
+
+            Console.WriteLine("Withdrawing...");
+            acc1.Withdraw(100);
+            Console.WriteLine(acc1);
+            Console.WriteLine("");
+
+            Console.WriteLine("Depositing...");
+            acc1.Deposit(150);
+            Console.WriteLine(acc1);
+            Console.WriteLine("");
+        }
+    }
+}

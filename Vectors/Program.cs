790edd19cdbed4a88470892d69e5bbdff990697e
Matheus Ávila <matheusjhavila@gmail.com>
2023-02-23 10:44:19 -0300

    List task added

---
+++ Vectors/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MyVector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many rooms will be rented? ");
+            int n = int.Parse(Console.ReadLine());
+
+            Rent[] room = new Rent[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Rent: #" + (i + 1));
+                Console.Write("Enter your name: ");
+                string name = Console.ReadLine(); 
+
+                Console.Write("Enter your email: ");
+                string email = Console.ReadLine();
+
+                Console.WriteLine("Choose your room (between 1 and 10): ");
+                int rentroom = int.Parse(Console.ReadLine());
+
+                room[rentroom] = new Rent(name, email);
+
+                Console.WriteLine("Name: " );
+                Console.WriteLine("Email: " );
+                Console.WriteLine("Room: " );
+            }
+
+        }
+    }
+}
---
+++ Vectors/Rent.cs
@@ -0,0 +1,23 @@
+﻿namespace MyVector
+{
+    internal class Rent
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Room { get; set; }
+
+
+        public Rent (string name, string email, int room)
+        {
+            Name = name;
+            Email = email;
+            Room = room;
+        }
+
+        public override string ToString()
+        {
+            return Name + Email;
+        }
+
+    }
+}

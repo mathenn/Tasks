61a0715760ae40732054bebd35f9cbda818464b3
Matheus Ávila <matheusjhavila@gmail.com>
2023-03-16 16:46:28 -0300

    Enums and StringBuilder task

---
+++ EnumTasks2/Entities/Comment.cs
@@ -0,0 +1,12 @@
+﻿namespace EnumTasks2.Entities
+{
+    public class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}

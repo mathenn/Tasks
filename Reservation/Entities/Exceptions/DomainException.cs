4f1e257cbe5ef5581c11ac0611a5c3b4d9b4e88e
Matheus Ávila <matheusjhavila@gmail.com>
2023-04-17 07:00:33 -0300

    Exceptions task

---
+++ Reservation/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,12 @@
+﻿namespace ReservationSystem.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+    public DomainException(string message) : base(message)
+        {
+
+        }
+
+    }
+
+}

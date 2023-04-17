5053f7c26cc2f64ea3c57c5058ef302af2cc5943
Matheus Ávila <matheusjhavila@gmail.com>
2023-04-17 17:19:43 -0300

    ExceptionTask (Done)

---
+++ ExceptionTask/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,10 @@
+﻿namespace ExceptionTask.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ ExceptionTask/Program.cs
@@ -0,0 +1,44 @@
+﻿using ExceptionTask.Entities;
+using ExceptionTask.Entities.Exceptions;
+using System.Globalization;
+
+namespace ExceptionTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string name = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit: ");
+                double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Account acc = new Account(number, name, balance, withdrawLimit);
+                Console.WriteLine();
+
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                acc.Withdraw(amount);
+                Console.WriteLine();
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format error: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("error: " + e.Message);
+            }
+        }
+    }
+}

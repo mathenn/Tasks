3c2896abb6a5ddf1f2af0588318c80ded5f33bcf
Matheus Ávila <matheusjhavila@gmail.com>
2023-05-25 18:28:04 -0300

    Linq and Lambda practices

---
+++ LinqPractice/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using Course.Entities;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Course
+{
+    class Program
+    {
+
+        static void Print<T>(string message, IEnumerable<T> collection)
+        {
+            Console.WriteLine(message);
+            foreach(T obj in collection)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Category c1 = new Category() { Id = 1, Name = "Tools", Tier = 2 };
+            Category c2 = new Category() { Id = 2, Name = "Computers", Tier = 1 };
+            Category c3 = new Category() { Id = 3, Name = "Electronics", Tier = 3 };
+
+            List<Product> products = new List<Product>()
+            { 
+                new Product() { Id = 1, Name = "Computer", Price = 1100.0, Category = c2 },
+                new Product() { Id = 2, Name = "Hammer", Price = 90.0, Category = c1 },
+                new Product() { Id = 3, Name = "TV", Price = 1700.0, Category = c3 },
+                new Product() { Id = 4, Name = "Notebook", Price = 1300.0, Category = c2 },
+                new Product() { Id = 5, Name = "Saw", Price = 80.0, Category = c1 },
+                new Product() { Id = 6, Name = "Tablet", Price = 700.0, Category = c2 },
+                new Product() { Id = 7, Name = "Camera", Price = 700.0, Category = c3 },
+                new Product() { Id = 8, Name = "Printer", Price = 350.0, Category = c3 },
+                new Product() { Id = 9, Name = "MacBook", Price = 1800.0, Category = c2 },
+                new Product() { Id = 10, Name = "Sound Bar", Price = 700.0, Category = c3 },
+                new Product() { Id = 11, Name = "Level", Price = 70.0, Category = c1 }
+            };
+
+            var r1 = products.Where(p => p.Category.Tier == 1);
+            Print("TIER 1 AND PRICE < 900:", r1);
+
+            var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+            Print("NAME OS PRODUCTS FROM TOOLS CATEGORY:", r2);
+
+            var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price, CategoryName = p.Category.Name });
+            Print("NAME STARTED WITH 'C' AND ANNONYMOUS OBJECT:", r3);
+
+            var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(products => products.Name);
+            Print("TIER 1 ORDERLY BY PRICE THEN BY NAME:", r4);
+
+            var r5 = products.GroupBy(p => p.Category);
+            foreach(IGrouping<Category, Product> group in r5)
+            {
+                Console.WriteLine("Cateogory " + group.Key.Name + ":");
+                foreach(Product p in group)
+                {
+                    Console.WriteLine(p);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    } 
+}

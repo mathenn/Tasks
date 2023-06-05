1a4dfe083666cd6870115e03d4947e7048381e11
Matheus Ávila <matheusjhavila@gmail.com>
2023-06-05 01:16:46 -0300

    Linq exercise (done)

---
+++ Course-Linq/Entities/Employees.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Course.Entities
+{
+    public class Employees
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public double Salary { get; set; }
+
+        public Employees(string name, string email, double salary)
+        {
+            Name = name;
+            Email = email;
+            Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return Name
+            + ", "
+            + Email
+            + ", "
+            + Salary;
+        }
+    }
+}
---
+++ Course-Linq/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Course.Entities;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file path: ");
+            string path = Console.ReadLine();
+
+            List<Employees> list = new List<Employees>();
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] employee = sr.ReadLine().Split(",");
+                    string name = employee[0];
+                    string email = employee[1];
+                    double salary = double.Parse(employee[2], CultureInfo.InvariantCulture);
+                    list.Add(new Employees(name, email, salary));
+                }
+            }
+
+            Console.Write("Enter salary: ");
+            double value = double.Parse(Console.ReadLine());
+            var emails = list.Where(p => p.Salary > value).OrderBy(p => p.Name).Select(p => p.Email);
+            Console.WriteLine("Email of people whose salary is more than " + value + ":");
+            Console.WriteLine();
+
+            foreach (string email in emails)
+            {
+                Console.WriteLine(email);
+            }
+
+            Console.WriteLine();
+
+            var sum = list.Where(p => p.Name[0] == 'M').Select(p => p.Salary).Sum();
+            Console.Write("Sum of salary of people whose name starts with 'M':" + sum);
+            Console.WriteLine();
+        }
+    }
+}
